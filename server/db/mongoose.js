580258bfbefcc8507238c79a7a21bec6b4d02b36
Sunil <chemsp@gmail.com>
2018-08-29 16:05:58 -0400

    commit

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://sunil:sunil1234@ds137812.mlab.com:37812/todoapp',{ useNewUrlParser: true });
+
+var connectionString = {
+    mlab:"mongodb://sunil:sunil1234@ds137812.mlab.com:37812/todoapp",
+    localhost : "mongodb://localhost:27017/TodoApp"
+}
+mongoose.connect(process.env.PORT?connectionString.mlab : connectionString.localhost,{ useNewUrlParser: true });
 
 module.exports = {
     mongoose : mongoose
