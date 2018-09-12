eed94f676122ecd8dab8e4f355f51f5f69cfa4ca
Sunil <chemsp@gmail.com>
2018-09-12 10:24:07 -0400

    added configuration

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,32 @@
 
 var env = process.env.NODE_ENV || 'development';
+  env = env.trim();
+if(env === 'development' || env === 'test'){
+    var config = require('./config.json');
+    var envConfig  = config[env];
 
-
-var uri = "mongodb://sunil:sunil1234@ds137812.mlab.com:37812/todoapp"
-if(env === 'development'){
-    process.env.PORT = 3000;
-    uri = "mongodb://127.0.0.1:27017/TodoApp_1";
-    process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/TodoApp_1";
+    Object.keys(envConfig).forEach((key)=>{
+    process.env[key] = envConfig[key];
+    //process.env[keys] = envConfig.MONGODB_URI
     
-} else if(env.trim() === 'test'){
-    process.env.PORT = 3000;
-    uri = "mongodb://127.0.0.1:27017/TodoApp_1_Test";
-    process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/TodoApp_1_test"
+});
+} else {
+    
 }
-console.log(process.env.NODE_ENV);
-console.log( process.env.PORT);
-console.log( process.env.MONGODB_URI);
+
+
+
+// var uri = "mongodb://sunil:sunil1234@ds137812.mlab.com:37812/todoapp"
+// if(env === 'development'){
+//     process.env.PORT = 3000;
+//     uri = "mongodb://127.0.0.1:27017/TodoApp_1";
+//     process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/TodoApp_1";
+    
+// } else if(env.trim() === 'test'){
+//     process.env.PORT = 3000;
+//     uri = "mongodb://127.0.0.1:27017/TodoApp_1_Test";
+//     process.env.MONGODB_URI = "mongodb://127.0.0.1:27017/TodoApp_1_test"
+// }
+// console.log(process.env.NODE_ENV);
+// console.log( process.env.PORT);
+// console.log( process.env.MONGODB_URI);
